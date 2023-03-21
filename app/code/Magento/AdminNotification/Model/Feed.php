f821b6f705ab741639fb574dada342aa95e49c0c
Adam <adam.crossland@zero1.co.uk>
2023-03-21 13:41:36 +0000

    test

--- app/code/Magento/AdminNotification/Model/Feed.php
+++ app/code/Magento/AdminNotification/Model/Feed.php
@@ -3,7 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\AdminNotification\Model;
+namespace Magento\AdminNotification\Model; 
 
 use Laminas\Http\Request;
 use Magento\AdminNotification\Model\InboxFactory;
