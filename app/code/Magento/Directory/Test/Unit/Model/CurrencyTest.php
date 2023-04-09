5f8feb7fd499bbfcd81fe78f2530548d584c7cc5
Adam <adam.crossland@zero1.co.uk>
2023-04-09 15:28:45 +0000

    removing debug

--- app/code/Magento/Directory/Test/Unit/Model/CurrencyTest.php
+++ app/code/Magento/Directory/Test/Unit/Model/CurrencyTest.php
@@ -137,19 +137,6 @@ class CurrencyTest extends TestCase
         self::assertEquals($expected, $output);
     }
 
-    protected function strigToBinary($string)
-    {
-        $characters = str_split($string);
-     
-        $binary = [];
-        foreach ($characters as $character) {
-            $data = unpack('H*', $character);
-            $binary[] = base_convert($data[1], 16, 2);
-        }
-     
-        return implode(' ', $binary);    
-    }
-
     /**
      * Return data sets for testGetOutputFormat()
      *
