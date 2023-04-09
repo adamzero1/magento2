f20a4fcd377963214514f42481378ad96479de49
Adam <adam.crossland@zero1.co.uk>
2023-04-09 15:37:17 +0000

    work

--- app/code/Magento/AdminNotification/Controller/Adminhtml/Notification.php
+++ app/code/Magento/AdminNotification/Controller/Adminhtml/Notification.php
@@ -14,7 +14,7 @@ namespace Magento\AdminNotification\Controller\Adminhtml;
 abstract class Notification extends \Magento\Backend\App\AbstractAction
 {
     /**
-     * Authorization level of a basic admin session...
+     * Authorization level of a basic admin session
      */
     const ADMIN_RESOURCE = 'Magento_AdminNotification::show_list';
 }
